097c653050ccc870fd2a892b634b0cec6107e788
Santiago Hewett <santiago.hewett@stu.ocsb.ca>
2025-02-10 12:13:12 +0000

    Done Math Operators swift

---
+++ MathOperators.swift
@@ -0,0 +1,37 @@
+//  MathOperators.swift
+
+//
+
+//  Created by Santiago Hewett
+
+//  Created on 2025/02/07
+
+//  Version 1.0
+
+//  Copyright (c) 2025 . All rights reserved.
+
+//
+
+//  This program display different math problems to the user.
+
+import Foundation  // Imports the Foundation module
+
+// Prints the message to the console
+// Addition
+print("5 + 7 = \(5 + 7)")
+// Subtraction
+print("5 - 6 = \(5 - 6)")
+// Multiplication
+print("3 * 7 = \(3 * 7)")
+// Division with integers
+print("10 / 5 = \(10 / 5)")
+// Division with float numbers
+print("1 / 3 = \(1.0 / 3.0)")
+// Division with double numbers
+print("1 / 3 = \(Double(1) / Double(3))")
+// Exponents to the power of 2
+print("4^2 = \(pow(4.0, 2.0))")
+// Exponents to the power of 3
+print("4^3 = \(pow(4.0, 3.0))")
+// Square root
+print("√36 = \(sqrt(36.0))")
